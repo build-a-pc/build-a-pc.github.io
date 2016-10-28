e4743d26696d01ca29b4f88ef04666ed10d10a20
Virkz <prein31@gmail.com>
2016-10-28 07:55:20 +0200

    search now find matching word in content.txt pages

--- scripts/search.js
+++ scripts/search.js
@@ -5,38 +5,55 @@ var same = "";
 var count = 0;
 var plus = 1;
 
-function readTextFile(file)
-{
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                alert(allText);
-            }
-        }
+function matchFound(searchString,page){
+	
+}
+
+function loadDoc(searchString,page) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+		var fileString = this.responseText;
+		var cleanString = fileString.replace(/[\?\|!"#¤%&\/\(\)=\+-\.,_'¨\^}{\[\]\$£@:;*`~€<>§]| (?= )/g, "")
+		var word = []
+		var lines = cleanString.split("\n");
+		var words = []
+		for (var i = 0; i < lines.length; i++){
+			words.push(lines[i].split(" "));
+		}
+		for(var i = 0; i < words.length; i++){
+			for(var j = 0; j < words[i].length; j++){
+				word.push(words[i][j]);
+			}
+		}
+		for (var i = 0; i < word.length; i++){
+			if(word[i]==searchString){
+				console.log('We have a match');
+				matchFound(searchString,page);
+			}
+		}
     }
-    rawFile.send(null);
+  };
+  xhttp.open("GET", "https://build\-a\-pc\.github\.io\\content.txt", true);
+  xhttp.send();
 }
 
 function checkTextBox(){
-	var userName = document.getElementById('fName');
-	if(userName.value !== ''){
-		console.log(userName.value);
-		if(same == userName.value){
+	var page = 1;
+	var searchString = document.getElementById('search');
+	if(searchString.value !== ''){
+		console.log(searchString.value);
+		if(same == searchString.value){
 			count = count + plus;
 		}
-		same = userName.value;
+		same = searchString.value;
 	}
 	if(count == 2){
 		clearInterval(setInterval);
-		console.log('2 times');
+		console.log('3 times');
 		same = "";
 		count = 0;
-		readTextFile("https://build\-a\-pc\.github\.io\\content.txt")
+		//readTextFile("https://build\-a\-pc\.github\.io\\content.txt")//
+		loadDoc(searchString.value,page);
 	}
-};// Search //
+};
