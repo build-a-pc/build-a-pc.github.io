f1f93303a11a515d3ab6700b803987043dee8683
Virkz <prein31@gmail.com>
2016-11-11 14:23:53 +0100

    Last edit, now it works 100%

--- scripts/search.js
+++ scripts/search.js
@@ -1,3 +1,9 @@
+/*
+FILE NAME: search.js
+WRITTEN BY: Petter
+WHEN: 2016 autumn
+PURPOSE: search through the website for word the user want to find.
+*/
 
 let Timer;
 let Timer2;
@@ -10,6 +16,7 @@ let pages = [];
 let show;
 loadDoc(pages,"content.txt",0,0);
 
+//Populating the dropdown with links to the pages found
 function dropDown(whatPage,splitWord){
   let div = document.querySelector(".search #dropdown");
   div.innerHTML = "";
@@ -39,21 +46,21 @@ function dropDown(whatPage,splitWord){
       a.innerHTML = ("Section: " + containPageNameMoreThan1Word(pageLink[0]));
       div.appendChild(a);
     }
-    if((pageLink.length == 0)){
-      let a = document.createElement("a");
-      a.innerHTML = ("No hits, try remove words or search on something else");
-      div.appendChild(a);
-    }	
   }
+  if((whatPage.length === 0)){
+    let a = document.createElement("a");
+    a.innerHTML = ("No hits, try remove words or search on something else");
+    div.appendChild(a);
+  }	
   let show = document.querySelector(".search #dropdown");
   show.style.display = "block";
 }
-
+//Checks if the page link consist of more than 1 word
 function containPageNameMoreThan1Word(pageLink){
   let splitPageLink = pageLink.split("_");
   for(let i = 0; i < splitPageLink.length; i++){
 	if(splitPageLink[i].length < 4){
-	  splitPageLink[i] = splitPageLink[i] = splitPageLink[i].toUpperCase();;
+	  splitPageLink[i] = splitPageLink[i] = splitPageLink[i].toUpperCase();
 	}
 	if(splitPageLink[i].length >= 4){
 	  splitPageLink[i] = splitPageLink[i].charAt(0).toUpperCase() + splitPageLink[i].slice(1);		
@@ -73,7 +80,7 @@ function containPageNameMoreThan1Word(pageLink){
   }
 	
 }
-
+//Takes the search string, split it up, take one word search every page for hit. Then compere pages with hits and sees if every word is on the same page
 function close1Index(searchString){
   let splitWord = searchString.split(" ");
   let hits = [];
@@ -91,21 +98,31 @@ function close1Index(searchString){
   }
   let sorted_hits = hits.slice().sort();
   let whatPage = [];
-  for (let i = 0; i < hits.length - 1; i++) {
-    if (sorted_hits[i + 1] == sorted_hits[i]) {
-        whatPage.push(sorted_hits[i]);
+  let pageNumberFound = "";
+  let pageFound = 0;
+  for (let i = 0; i < hits.length; i++) {
+	if(pageNumberFound != sorted_hits[i]){
+		pageNumberFound = sorted_hits[i];
+		pageFound = 0;
+	}
+	if(pageNumberFound == sorted_hits[i]){
+		pageFound = pageFound + 1;
+	}
+    if(pageFound == splitWord.length){
+      whatPage.push(sorted_hits[i]);
     }
   }
   dropDown(whatPage,splitWord);
 }
 
+//loads the page, and save it as a array containing every word
 function loadDoc(listSave,page,numberOfPage,run) {
   let fileString;
   let xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
     let page1 = [];
-	if(run == 0){
+	if(run === 0){
 		fileString = this.responseText;
 	}
     if(run >= 1){
@@ -133,14 +150,14 @@ function loadDoc(listSave,page,numberOfPage,run) {
   xhttp.open("GET", "https://build\-a\-pc\.github\.io\\"+page, true);
   xhttp.send();
 }
-
+//Extract the body from a page
 function exractBody(response) {
   let dom1 = document.createElement("html");
   dom1.innerHTML = response;
   let bodyText = dom1.getElementsByTagName("body")[0].innerHTML;
   return bodyText;
 }
-
+//Sees if the same thing has been stored in the input field for the search more than 3 seconds, then exute the rest
 function checkTextBox(){
   let searchString = document.getElementById("search-bar");
   if(searchString.value !== ""){
@@ -160,7 +177,7 @@ function checkTextBox(){
     }
 }
 
-
+//starts the checking of the input field
 function start(){
   if(!started){
     started = true;
@@ -172,14 +189,14 @@ function start(){
   let show = document.querySelector(".search #dropdown");
   show.style.display = "none";
 }
-
+//Stops the checking of the input field
 function stop(){
   started = false;
   clearInterval(Timer);
   show = document.querySelector(".search #dropdown");
   Timer2 = setInterval(hideDropDown, 500);
 }
-
+//hides the dropdown
 function hideDropDown(){
   show.style.display = "none";
   clearInterval(Timer2);
