32efdfb2c1da5236dd41324648144cc03c7371e2
Thomas Haugan <thomas.haugan@ntnu.no>
2016-11-13 12:34:04 +0100

    bloody hell!

--- scripts/breadcrumbs.js
+++ scripts/breadcrumbs.js
@@ -5,93 +5,73 @@ WHEN: 2016 autumn
 PURPOSE: Dynamic update of breadcrumb trail
 */
 
-console.log("\n\n******  RUN   BREADCRUMBS  ******");
-console.log("\t hello world ....");
-console.log("\n\n*******************************************");
+//  run all functions on top while having function declarations on bottom (hoisted)
+window.addEventListener(EVENT_ON_LOADED, 
+  function(ev){ updateBreadCrumbs(ev)} );
 
 
- let visibleState = false;
- let displayState = false;
 
+function updateBreadCrumbs(){
 
-window.addEventListener("baponcontentloaded", 
-	function(ev){
-	  console.log("baponcontent  LOADED ");
-	  let temp = ev.detail;
-	  console.log("temp = " + temp);
-	  console.log("ev.detail = " + ev.detail);
-	  //breadcrumsDisplayedstate();
-	  }
-)
-
-// TMP onloaded example
-window.addEventListener(EVENT_ON_LOADED, function(ev) {
-
-	breadcrumsDisplayedstate();
-    console.log("\nEVENT_ON_LOADED");
-    let options = ev.detail;
-	
+	let eventus = arguments[0];
 	let bread_tag = document.getElementById("breadcrumbs");
+	displayElement(bread_tag);
+	
 	let bread_list = bread_tag.getElementsByTagName("ul");
 	bread_list[0].innerHTML = "";	
 	
-	let li_home = document.createElement("LI");
-	let anc_home = document.createElement("A");	
-	li_home.appendChild(anc_home);
-	let txt_home = document.createTextNode("Home");
-	anc_home.setAttribute("href", FILE_PROTOCOL_PAGE_ROOT);
-	anc_home.appendChild(txt_home);
-	bread_list[0].appendChild(li_home);
+	var section = eventus.detail.section;
+	var sectionname = eventus.detail.section_name;
+	var page = eventus.detail.page;
+	var pagename = eventus.detail.page_name;
+		
+	var link = 	FILE_PROTOCOL_PAGE_ROOT;
+	addBreadCrumb(link, "Home", bread_list);	
+		
+	let query = "";
 	
-	let sectionName = options.section_name;
-	let pageName = options.page_name;	
-		
-	
-	if(sectionName){
-		let li_sect = document.createElement("LI");
-		var anc_sect = document.createElement("A");
-    	li_sect.appendChild(anc_sect);
-		
-		var txt_sect = document.createTextNode(sectionName);
-		anc_sect.setAttribute("href", "http://www.vg.no");
-		anc_sect.appendChild(txt_sect);
-		bread_list[0].appendChild(li_sect);
-	}	
-	if(pageName){
-		let li_page = document.createElement("LI");
-		var anc_page = document.createElement("A");
-		li_page.appendChild(anc_page);
-		anc_page.setAttribute("href", "http://www.dagbladet.no");
-		
-		var txt_page = document.createTextNode(pageName);
-		anc_page.appendChild(txt_page);
-		bread_list[0].appendChild(li_page);
-		console.log("Blink="+li_page.innerHTML);
+	if(section && !page || page && page == section){
+		query = "?section=" + section;
+		link = link + query;
+		addBreadCrumb(link, sectionname, bread_list);
 	}
-		
-	
-	if(sectionName){
-		console.log("1>" + txt_page.textContent + " 2>" + txt_sect.textContent);
+	else if(!section && page){
+		query = "?page=" + page;
+		link = link + query;
+		addBreadCrumb(link, pagename, bread_list);
 	}
-	else{
-		console.log("s1>" + txt_page.textContent);
-		bread_list[0].removeChild(bread_list[0].lastChild);
-	}
-	
-	if(true){
-	console.log("Zzz# " + (bread_list.length-1));
-	}
-	
-		
-	
-  }
-);
+	else if(section && page){	
+		query = "?section=" + section;
+		link = link + query;
+		addBreadCrumb(link, sectionname, bread_list);
+		query = query  + "&page=" + page;
+		link = link + query;
+		addBreadCrumb(link, pagename, bread_list);	
+	}	  
+}
 
+// Generic/modular extraction query from URL and return table with name/value-pairs
+function getQueryPairs(){
+	var query = window.location.search.substring(1);
+    var queryTab = query.split("&");
+    return queryTab;
+}
 
+// Append a hyperlinked breadcrumb to BrCr-trail
+function addBreadCrumb(weblink, brCrName, brCrList){
+	var list_element = document.createElement("LI");
+	var anchor_element = document.createElement("A");
+	list_element.appendChild(anchor_element);
+	anchor_element.setAttribute("href", weblink);
+		
+	var txt_node = document.createTextNode(brCrName);
+	anchor_element.appendChild(txt_node);
+	brCrList[0].appendChild(list_element);
+}
 
 // Show/Hide breadcrumbs trail
-function breadcrumsDisplayedstate() {
-	let styleBrCr = document.getElementById("breadcrumbs").style;
-	styleBrCr.display = "block";  
-	styleBrCr.visibility = "visible";
+function displayElement(elementID){
+	var styleElement = elementID.style;
+	styleElement.display = "block";  
+	styleElement.visibility = "visible";
 }
